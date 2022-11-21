a26b7e14c1db28967d5dfde0b0f29273ef901b3a
TiavinaTiTi <titus50231@gmail.com>
2022-11-21 14:33:56 +0300

    Pokemon Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { POKEMONS } from './moke-pokemon';
+import { Pokemon } from './pokemon';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'projetPokemon';
+export class AppComponent implements OnInit{
+  pokemonList: Pokemon[] = POKEMONS;
+  pokemonSelected: Pokemon|undefined;
+
+
+  ngOnInit(): void {
+    console.table(this.pokemonList);
+    // this.selectPokemon(this.pokemonList[4]);
+  }
+  selectPokemon(pokemonId: String){
+    const pokemon: Pokemon|undefined = this.pokemonList.find(pokemon => pokemon.id == +pokemonId)
+    if(pokemon){
+      console.log(`Vous avez Clic sur :${pokemon.name}`);
+      this.pokemonSelected = pokemon;
+    }else{
+      console.log(`Pokemon n'existe pas.`);
+      this.pokemonSelected = pokemon;
+    }
+  }
+
+
 }
+
+// Pseudo evenement Angular (Evenement JS)
